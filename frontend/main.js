e67dd6d59daf9cd63dfa0f4ffe1668f42a2cef02
Kien Nguyen <kiennguyn17@gmail.com>
2023-03-11 18:10:49 +1100

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -3,11 +3,11 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 });
 
 const localFunctionURL = 'http://localhost:7071/api/Counter';
-const functionApiURL = 'http://localhost:7071/api/Counter';
+const functionApiURL = 'https://getresumekien.azurewebsites.net/api/GetResumeCounter?code=IJIufamrP5WPW0J2EJLSzw7jEbUnbylqs-ESuZg5luavAzFu7s4rIQ==';
 
 const getVisitCount = () => {
 	let count = 30;
-	fetch(localFunctionURL).then(response => {
+	fetch(functionApiURL).then(response => {
 			return response.json()
 	}).then(response =>{
 		console.log("Website called function API.");
